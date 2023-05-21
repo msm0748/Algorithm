65971bac72cc4017c901969a6b697ac2256481cd
Moon Seokmin <78075709+msm0748@users.noreply.github.com>
2023-05-21 12:34:43 +0900

    [level 0] Title: 모스부호 (1), Time: 0.18 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120838. 모스부호 （1）/모스부호 （1）.js
@@ -0,0 +1,16 @@
+function solution(letter) {
+    var answer = letter.split(" ");
+    var result = "";
+    var morse = { 
+    '.-':'a','-...':'b','-.-.':'c','-..':'d','.':'e','..-.':'f',
+    '--.':'g','....':'h','..':'i','.---':'j','-.-':'k','.-..':'l',
+    '--':'m','-.':'n','---':'o','.--.':'p','--.-':'q','.-.':'r',
+    '...':'s','-':'t','..-':'u','...-':'v','.--':'w','-..-':'x',
+    '-.--':'y','--..':'z'
+    }
+    for(const val of answer){
+        result += morse[val];
+    }
+    
+    return result;
+}
