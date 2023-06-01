fc390d4bd819b3aff6fd9f1ae2a8e586b460f482
Moon Seokmin <78075709+msm0748@users.noreply.github.com>
2023-06-01 20:47:14 +0900

    [level 0] Title: 팩토리얼, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120848. 팩토리얼/팩토리얼.js
@@ -0,0 +1,9 @@
+function solution(n) {
+    var answer = 1;
+    for(let i=1; i <= n; i++){
+        answer *= i;
+        if(answer === n){ return i}
+        if(answer > n){ return i-1}
+    }
+    return answer;
+}
