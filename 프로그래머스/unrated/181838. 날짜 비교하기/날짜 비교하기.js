b0e6b994363f381bb8fe5ab3ec86b02370bc037b
Moon Seokmin <78075709+msm0748@users.noreply.github.com>
2023-06-04 18:04:58 +0900

    [unrated] Title: 날짜 비교하기, Time: 0.11 ms, Memory: 33.9 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181838. 날짜 비교하기/날짜 비교하기.js
@@ -0,0 +1,6 @@
+function solution(date1, date2) {
+    var answer = 0;
+    date1 = new Date(date1[0], date1[1], date1[2]);
+    date2 = new Date(date2[0], date2[1], date2[2]);
+    return date1 < date2 ? 1 : 0;
+}
