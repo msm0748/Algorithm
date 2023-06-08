a2de6eaaa81cf985d7cef586a090d564f81d1411
Moon Seokmin <78075709+msm0748@users.noreply.github.com>
2023-06-08 20:06:30 +0900

    [Bronze V] Title: 나머지, Time: 140 ms, Memory: 9588 KB -BaekjoonHub

---
+++ 백준/Bronze/10430. 나머지/나머지.js
@@ -0,0 +1,9 @@
+//첫째 줄에 (A+B)%C, 둘째 줄에 ((A%C) + (B%C))%C, 셋째 줄에 (A×B)%C, 넷째 줄에 ((A%C) × (B%C))%C를 출력한다.
+let input = require('fs').readFileSync('/dev/stdin').toString().split(" ");
+let A = parseInt(input[0]);
+let B = parseInt(input[1]);
+let C = parseInt(input[2]);
+console.log((A + B) % C);
+console.log(((A % C) + (B % C)) % C);
+console.log((A * B) % C);
+console.log(((A % C) * (B % C)) % C);
