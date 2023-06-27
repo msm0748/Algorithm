70ddf5e7beab6dbb3bed54169088927798371fa4
Moon Seokmin <78075709+msm0748@users.noreply.github.com>
2023-06-27 19:17:45 +0900

    [unrated] Title: 문자열 돌리기, Time: 52.99 ms, Memory: 32.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181945. 문자열 돌리기/문자열 돌리기.js
+++ 프로그래머스/unrated/181945. 문자열 돌리기/문자열 돌리기.js
@@ -9,8 +9,8 @@ let input = [];
 rl.on('line', function (line) {
     input = [line];
 }).on('close',function(){
-    str = input[0];
-    for(let i = 0; i < str.length; i++){
-        console.log(str[i])
+    const string = input[0];
+    for(const str of string){
+        console.log(str)
     }
 });
