dd0b569cf6a67d4cb67531b2ac32c10dc2192a0e
Moon Seokmin <78075709+msm0748@users.noreply.github.com>
2023-06-28 19:53:17 +0900

    [unrated] Title: 수 조작하기 1, Time: 3.39 ms, Memory: 37.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181926. 수 조작하기 1/수 조작하기 1.js
+++ 프로그래머스/unrated/181926. 수 조작하기 1/수 조작하기 1.js
@@ -1,16 +1,19 @@
 function solution(n, control) {
-    var answer = n;
-    for(let i = 0; i < control.length; i++){
-        if(control[i] === "w"){
-            answer += 1;
-        }else if(control[i] === "s"){
-            answer -= 1;
-        }else if(control[i] === "d"){
-            answer += 10;
-        }else if(control[i] === "a"){
-            answer -= 10;
+    for(const str of control){
+        switch(str){
+            case "w":
+                n += 1;
+                break;
+            case "s":
+                n -= 1;
+                break;
+            case "d":
+                n += 10;
+                break;
+            case "a":
+                n -= 10;
+                break;
         }
     }
-
-    return answer;
+    return n;
 }
