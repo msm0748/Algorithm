65cb0377cf199649ca5504e0f938081197539a42
Moon Seokmin <78075709+msm0748@users.noreply.github.com>
2023-06-28 19:43:25 +0900

    [unrated] Title: 이어 붙인 수, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181928. 이어 붙인 수/이어 붙인 수.js
+++ 프로그래머스/unrated/181928. 이어 붙인 수/이어 붙인 수.js
@@ -1,12 +1,12 @@
 function solution(num_list) {
     var even = "";
     var odd = "";
-    for(let i = 0; i < num_list.length; i++){
+    num_list.forEach((v, i) => {
         if(num_list[i] % 2 === 0){
-            even += String(num_list[i])
+            even += String(num_list[i]);
         }else{
-            odd += String(num_list[i])
+            odd += String(num_list[i]);
         }
-    }
+    });
     return Number(even) + Number(odd);
 }
