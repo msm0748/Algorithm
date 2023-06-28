ddf2edaa1253e641a377ff21c3864e3239b6a16f
Moon Seokmin <78075709+msm0748@users.noreply.github.com>
2023-06-28 19:40:48 +0900

    [unrated] Title: 원소들의 곱과 합, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181929. 원소들의 곱과 합/원소들의 곱과 합.js
+++ 프로그래머스/unrated/181929. 원소들의 곱과 합/원소들의 곱과 합.js
@@ -1,12 +1,10 @@
 function solution(num_list) {
-    var num1 = 1;
-    var num2 = 0;
+    let product = 1;
+    let sum = 0;
     for(let i = 0; i < num_list.length; i++){
-        num1 *= num_list[i]
-    }
-    for(let i = 0; i < num_list.length; i++){
-        num2 += num_list[i]
+        product *= num_list[i];
+        sum += num_list[i];
     }
     
-    return num1 > Math.pow(num2, 2) ? 0 : 1;
+    return product > Math.pow(sum, 2) ? 0 : 1;
 }
