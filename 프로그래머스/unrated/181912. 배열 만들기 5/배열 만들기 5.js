da4dcf5e60a8ea2076b898b360004d750cf81591
Moon Seokmin <78075709+msm0748@users.noreply.github.com>
2023-06-30 08:09:28 +0900

    [unrated] Title: 배열 만들기 5, Time: 1.24 ms, Memory: 36.8 MB -BaekjoonHub

--- 프로그래머스/unrated/181912. 배열 만들기 5/배열 만들기 5.js
+++ 프로그래머스/unrated/181912. 배열 만들기 5/배열 만들기 5.js
@@ -1,8 +1,9 @@
 function solution(intStrs, k, s, l) {
     var answer = [];
     for(const val of intStrs){
-        if(Number(val.substr(s, l)) > k){
-            answer.push(Number(val.substr(s, l)))
+        intStrs = Number(val.substr(s, l));
+        if(intStrs > k){
+            answer.push(intStrs)
         }
     }
     return answer;
