a8b8a2a83beec0d60f6af80bdbd33b5ccf83b43c
Moon Seokmin <78075709+msm0748@users.noreply.github.com>
2023-06-29 23:54:34 +0900

    [unrated] Title: 문자 개수 세기, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181902. 문자 개수 세기/문자 개수 세기.js
+++ 프로그래머스/unrated/181902. 문자 개수 세기/문자 개수 세기.js
@@ -1,9 +1,6 @@
 function solution(my_string) {
-    var answer = [];
     var abc = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-    for(const val of abc){
-        answer.push(0);
-    }
+    var answer = [...abc].map(v => 0);
     for(const string of my_string){
         const index = abc.indexOf(string)
         answer[index] += 1;
