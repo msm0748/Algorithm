7f6f9e0c121fdad9784de8c77746685ea3cfca04
Moon Seokmin <78075709+msm0748@users.noreply.github.com>
2023-06-30 18:50:14 +0900

    [unrated] Title: rny_string, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181863. rny＿string/rny＿string.js
+++ 프로그래머스/unrated/181863. rny＿string/rny＿string.js
@@ -1,11 +1,4 @@
 function solution(rny_string) {
-    var answer = '';
-    for(const val of rny_string){
-        if(val === "m"){
-            answer += "rn"
-        }else{
-            answer += val
-        }
-    }
-    return answer;
+    var answer = [...rny_string].map(v => v === "m" ? "rn" : v);
+    return answer.join("");
 }
