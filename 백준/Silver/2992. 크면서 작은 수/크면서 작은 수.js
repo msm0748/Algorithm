0030129808798c474d7cfe65c85ee2eb1ba7e0c9
Moon Seokmin <78075709+msm0748@users.noreply.github.com>
2023-09-05 16:43:50 +0900

    [Silver III] Title: 크면서 작은 수, Time: 112 ms, Memory: 9328 KB -BaekjoonHub

--- 백준/Silver/2992. 크면서 작은 수/크면서 작은 수.js
+++ 백준/Silver/2992. 크면서 작은 수/크면서 작은 수.js
@@ -1,20 +1,29 @@
 const input = require('fs').readFileSync('/dev/stdin').toString().trim();
 
-const permutation = (permu, rests, output) => {
-  if (rests.length == 0) {
-    output.push(permu.join('')); // 1차원 배열로 조인하여 결과에 추가
-    return;
+const nextPermutation = (arr) => {
+  let i = arr.length - 1;
+  while (i > 0 && arr[i - 1] >= arr[i]) i--;
+
+  if (i <= 0) return false;
+
+  let j = arr.length - 1;
+  while (arr[j] <= arr[i - 1]) j--;
+
+  [arr[i - 1], arr[j]] = [arr[j], arr[i - 1]];
+
+  j = arr.length - 1;
+  while (i < j) {
+    [arr[i], arr[j]] = [arr[j], arr[i]];
+    i++;
+    j--;
   }
-  rests.forEach((v, idx) => {
-    const rest = [...rests.slice(0, idx), ...rests.slice(idx + 1)];
-    permutation([...permu, v], rest, output);
-  });
+
+  return true;
 };
 
-const output = [];
 const arr = input.split('');
-permutation([], arr, output);
-
-const result = output.sort((a, b) => a - b).find((num) => input < num);
-
-console.log(result ? result : 0); // 1차원 배열 출력
+if (nextPermutation(arr)) {
+  console.log(arr.join(''));
+} else {
+  console.log(0);
+}
